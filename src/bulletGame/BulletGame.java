4088166b26dc0092d507d8cd2e1898001fd35f1b
takkasila <takkasila@outlook.com>
2014-09-13 02:10:54 +0700

    Setting entities in BulletGame Added test Bullets in Entities

--- src/bulletGame/BulletGame.java
+++ src/bulletGame/BulletGame.java
@@ -1,5 +1,7 @@
 package bulletGame;
 
+import java.util.LinkedList;
+
 import javax.security.auth.login.AppConfigurationEntry;
 
 import org.newdawn.slick.AppGameContainer;
@@ -14,37 +16,44 @@ public class BulletGame extends BasicGame {
 	final static int SCREEN_WIDTH = 800;
 	final static int SCREEN_HEIGHT = 600;
 
+	LinkedList<Entity> entities;
+	
 	public BulletGame(String title) {
 		super(title);
+		entities = new LinkedList<>();
 	}
 
 	public static void main(String[] args) throws SlickException {
 
 		BulletGame game = new BulletGame("Bullet Game");
-		
 		AppGameContainer appgc = new AppGameContainer(game);
 		
 		appgc.setDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, false);
-		
 		appgc.setTargetFrameRate(60);
 		appgc.start();
 	}
 
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		
-		
-		
-	}
-
-	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		
+		
+	}
+	@Override
+	public void render(GameContainer arg0, Graphics graphic) throws SlickException {
+		
+		for(Entity entity : entities)
+		{
+			entity.Render(graphic);
+		}
+		
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		
+	public void update(GameContainer arg0, int delta) throws SlickException {
+		for(Entity entity : entities)
+		{
+			entity.Update(delta);
+		}
 	}
 
 }
